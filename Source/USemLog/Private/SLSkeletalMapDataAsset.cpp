4de355800e260cbf318898489f32e86f2de1d36a
Andrei Haidu <andrei.haidu@yahoo.com>
2018-11-16 09:26:31 +0100

    fix from Johannes to read out the skeletal bone namesof the MapDataAsset

--- Source/USemLog/Private/SLSkeletalMapDataAsset.cpp
+++ Source/USemLog/Private/SLSkeletalMapDataAsset.cpp
@@ -2,46 +2,32 @@
 // Author: Andrei Haidu (http://haidu.eu)
 
 #include "SLSkeletalMapDataAsset.h"
-#include "Animation/Rig.h"
 
 #if WITH_EDITOR
 // Called when a property is changed in the editor
 void USLSkeletalMapDataAsset::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
-	UE_LOG(LogTemp, Warning, TEXT(">> %s::%d"), TEXT(__FUNCTION__), __LINE__);
 
 	// Get the changed property name
 	FName PropertyName = (PropertyChangedEvent.Property != NULL) ?
 		PropertyChangedEvent.Property->GetFName() : NAME_None;
 
-	if (PropertyName == GET_MEMBER_NAME_CHECKED(USLSkeletalMapDataAsset, Skeleton))
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(USLSkeletalMapDataAsset, SkeletalMesh))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("\t %s::%d"), TEXT(__FUNCTION__), __LINE__);
-		TArray<FName> NodesName;
-		int32 NrOfBones  = Skeleton->GetMappedValidNodes(NodesName);
-		UE_LOG(LogTemp, Warning, TEXT(">> %s::%d NR bones:%d"), TEXT(__FUNCTION__), __LINE__, NrOfBones);
-		
-		URig* Rig = Skeleton->GetRig();
-		if (Rig)
-		{
-			TArray<FNode> Nodes = Rig->GetNodes();
-			for (const auto& Node : Nodes)
-			{
-				FName NName = Node.Name;
-				FName NPName = Node.ParentName;
-				FString NDName = Node.DisplayName;
-				UE_LOG(LogTemp, Warning, TEXT("\t\t %s::%d Name=%s, ParentName=%s, DisplayName=%s "),
-					TEXT(__FUNCTION__), __LINE__, *NName.ToString(), *NPName.ToString(), *NDName);
-				BoneClassMap.Add(Node.Name, "NONE");			
-			}
-		}
-		
-	}
-	else if (PropertyName == GET_MEMBER_NAME_CHECKED(USLSkeletalMapDataAsset, SkeletalMesh))
-	{
-		
+		// Empty previous map
 		BoneClassMap.Empty();
+
+		// Create a temporary skeletal mesh component from the mesh to read the bone names
+		USkeletalMeshComponent* SkelMeshComp = NewObject<USkeletalMeshComponent>(this);
+		SkelMeshComp->SetSkeletalMesh(SkeletalMesh);
+		TArray<FName> BoneNames;
+		SkelMeshComp->GetBoneNames(BoneNames);
+		for (auto Name : BoneNames)
+		{
+			BoneClassMap.Add(Name, "");
+		}
+		SkelMeshComp->DestroyComponent();
 	}
 }
 #endif // WITH_EDITOR
