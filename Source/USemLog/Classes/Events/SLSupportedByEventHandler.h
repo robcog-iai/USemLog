e86aaa48d7c654146b75698c2f358bf4f92e96f2
Andrei Haidu <andrei.haidu@yahoo.com>
2019-04-04 11:16:14 +0200

    added year range to copyright

--- Source/USemLog/Classes/Events/SLSupportedByEventHandler.h
+++ Source/USemLog/Classes/Events/SLSupportedByEventHandler.h
@@ -1,5 +1,5 @@
 #pragma once
-// Copyright 2019, Institute for Artificial Intelligence - University of Bremen
+// Copyright 2017-2019, Institute for Artificial Intelligence - University of Bremen
 // Author: Andrei Haidu (http://haidu.eu)
 
 #pragma once
