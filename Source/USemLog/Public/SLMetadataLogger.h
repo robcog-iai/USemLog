bb10d608abc106f4f3bd18c29bdbdda820d1516b
Andrei Haidu <andrei.haidu@yahoo.com>
2019-10-21 15:06:19 +0200

    added metadata logger template

---
+++ Source/USemLog/Public/SLMetadataLogger.h
@@ -0,0 +1,17 @@
+// Copyright 2019, Institute for Artificial Intelligence - University of Bremen
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SLMetadataLogger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class USEMLOG_API USLMetadataLogger : public UObject
+{
+	GENERATED_BODY()
+	
+};
