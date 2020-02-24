739fbb2de20f586321c20a63f3d5dc85506c0068
Andrei Haidu <andrei.haidu@yahoo.com>
2020-02-24 11:58:28 +0100

    rm left right hand forcedd scale

--- Source/USemLog/Private/Vision/SLVisionPoseableMeshActor.cpp
+++ Source/USemLog/Private/Vision/SLVisionPoseableMeshActor.cpp
@@ -45,17 +45,19 @@ void ASLVisionPoseableMeshActor::SetBoneTransforms(const TMap<FName, FTransform>
 {
 	for(const auto& Pair : BoneTransfroms)
 	{
-		// TODO hack to cut off hands
-		if (Pair.Key.IsEqual(FName("LeftHand")) || Pair.Key.IsEqual(FName("RightHand")))
-		{
-			FTransform Scale0(Pair.Value);
-			Scale0.SetScale3D(FVector(0.f));
-			PoseableMeshComponent->SetBoneTransformByName(Pair.Key, Scale0, EBoneSpaces::WorldSpace);
-		}
-		else
-		{
-			PoseableMeshComponent->SetBoneTransformByName(Pair.Key, Pair.Value, EBoneSpaces::WorldSpace);
-		}
+		PoseableMeshComponent->SetBoneTransformByName(Pair.Key, Pair.Value, EBoneSpaces::WorldSpace);
+		
+		//// Hack to cut off hands
+		//if (Pair.Key.IsEqual(FName("LeftHand")) || Pair.Key.IsEqual(FName("RightHand")))
+		//{
+		//	FTransform Scale0(Pair.Value);
+		//	Scale0.SetScale3D(FVector(0.f));
+		//	PoseableMeshComponent->SetBoneTransformByName(Pair.Key, Scale0, EBoneSpaces::WorldSpace);
+		//}
+		//else
+		//{
+		//	PoseableMeshComponent->SetBoneTransformByName(Pair.Key, Pair.Value, EBoneSpaces::WorldSpace);
+		//}
 	}
 }
 
