611babb95edb19f0668f05eb8487d9612e12765d
Andrei Haidu <andrei.haidu@yahoo.com>
2020-07-24 10:55:20 +0200

    knowrob connection client actor dummy added

---
+++ Source/USemLog/Private/Knowrob/KRConnClient.cpp
@@ -0,0 +1,27 @@
+// Copyright 2017-2020, Institute for Artificial Intelligence - University of Bremen
+
+
+#include "KRConnClient.h"
+
+// Sets default values
+AKRConnClient::AKRConnClient()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AKRConnClient::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AKRConnClient::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
