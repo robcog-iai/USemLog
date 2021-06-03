43f8c7c0f63b0e67ea1f0534a2d59653337f175c
Andrei Haidu <andrei.haidu@yahoo.com>
2021-06-03 11:15:39 +0200

    added look at movement controller

---
+++ Source/USemLog/Private/Misc/SLLookAtAnim.cpp
@@ -0,0 +1,102 @@
+// Copyright 2017-2021, Institute for Artificial Intelligence - University of Bremen
+// Author: Andrei Haidu (http://haidu.eu)
+
+#include "SLLookAtAnim.h"
+#include "Engine/StaticMeshActor.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values for this component's properties
+USLLookAtAnim::USLLookAtAnim()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USLLookAtAnim::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (Init())
+	{
+		SetComponentTickEnabled(true);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s::%d %s: could not init component.."), *FString(__FUNCTION__), __LINE__, *GetName());
+	}
+	
+}
+
+
+// Called every frame
+void USLLookAtAnim::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	ApplyLookAt();
+}
+
+// Check if parent and parameters are valid
+bool USLLookAtAnim::Init()
+{
+	if (!LookAtActor || !LookAtActor->IsValidLowLevel())
+	{
+		if (SoftLookAtActor && SoftLookAtActor->IsValidLowLevel())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%s::%d %s: Using soft reference.."), *FString(__FUNCTION__), __LINE__, *GetName());
+			LookAtActor = SoftLookAtActor.Get();
+		}
+	}
+
+
+	if (LookAtActor && LookAtActor->IsValidLowLevel())
+	{
+		if (AStaticMeshActor* Act = Cast<AStaticMeshActor>(GetOwner()))
+		{
+			ParentSM = Act;
+			if (ParentSM->GetRootComponent()->Mobility == EComponentMobility::Movable)
+			{
+				bIsInit = true;
+				return true;
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("%s::%d %s: parent actor is not set to movable.."), *FString(__FUNCTION__), __LINE__, *GetName());
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("%s::%d %s: parent is not a static mesh actor.."), *FString(__FUNCTION__), __LINE__, *GetName());
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s::%d %s: no actor given to follow.."), *FString(__FUNCTION__), __LINE__, *GetName());
+	}
+	return false;
+}
+
+// Apply look at movement
+void USLLookAtAnim::ApplyLookAt()
+{
+	// Get the look at rotator
+	FVector Start = ParentSM->GetActorLocation();
+	FVector Target = LookAtActor->GetActorLocation();
+	FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(Start, Target);
+	FRotator TargetRot = ParentSM->GetActorRotation();
+	if (bPitch)
+	{
+		TargetRot.Pitch = LookAtRot.Pitch;
+	}
+	if (bYaw)
+	{
+		TargetRot.Yaw = LookAtRot.Yaw;
+	}
+	ParentSM->SetActorRotation(TargetRot);
+}
+
---
+++ Source/USemLog/Private/Misc/SLLookAtAnim.h
@@ -0,0 +1,64 @@
+// Copyright 2017-2021, Institute for Artificial Intelligence - University of Bremen
+// Author: Andrei Haidu (http://haidu.eu)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SLLookAtAnim.generated.h"
+
+// Forward declaration
+class AStaticMeshActor;
+
+/**
+ *
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), DisplayName = "SL Look At Anim")
+class USLLookAtAnim : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USLLookAtAnim();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Check if parent and parameters are valid
+	bool Init();
+
+	// Check if component is initialized
+	bool IsInit() const { return bIsInit; };
+
+private:
+	// Apply look at movement
+	void ApplyLookAt();
+
+private:
+	// True if the individual is succesfully created and initialized
+	bool bIsInit = false;
+
+	// Actor to follow
+	UPROPERTY(EditAnywhere, Category = "Semantic Logger")
+	AActor* LookAtActor;
+
+	UPROPERTY(EditAnywhere, Category = "Semantic Logger")
+	TSoftObjectPtr<AActor> SoftLookAtActor;
+
+	// Rotation axis
+	UPROPERTY(EditAnywhere, Category = "Semantic Logger")
+	bool bPitch = true;
+
+	// Rotation axis
+	UPROPERTY(EditAnywhere, Category = "Semantic Logger")
+	bool bYaw = true;
+
+	// Parent actor to move
+	AStaticMeshActor* ParentSM;
+};
