07b430442bee57ad5c7297af3c932b5104311e85
RHel <rhelmert2@gmail.com>
2023-03-24 16:41:01 +0100

    fix log output line throwing an error

--- Source/USemLog/Private/Monitors/SLContactMonitorInterface.cpp
+++ Source/USemLog/Private/Monitors/SLContactMonitorInterface.cpp
@@ -241,7 +241,7 @@ void ISLContactMonitorInterface::OnOverlapEnd(UPrimitiveComponent* OverlappedCom
 	USLBaseIndividual* OtherIndividual = FSLIndividualUtils::GetIndividualObject(OtherActor);
 	if (OtherIndividual == nullptr)
 	{
-		UE_LOG(LogTemp, Error, TEXT("%s::%d %s is not annotated, this should not happen.."), *FString(__FUNCTION__), __LINE__);
+		UE_LOG(LogTemp, Error, TEXT("%s::%d %s is not annotated, this should not happen.."), *FString(__FUNCTION__), __LINE__, *OtherActor->GetName());
 		return;
 	}
 
